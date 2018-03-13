6e0cf397f7e79c7f685c5c8441c7670938b8be83
superchijinpeng <superchijinpeng@gmail.com>
2018-03-13 15:42:07 +0800

    style(page): add bind page style

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,42 @@ export default {
   name: 'App'
 }
 </script>
+
+<style lang="scss">
+page {
+  background-color: #f8f8f8;
+  height: 100%;
+}
+
+input {
+  border: 1px solid #dcdfe6;
+  border-radius: 4px;
+  box-sizing: border-box;
+  font-size: 14px;
+  height: 40px;
+  line-height: 40px;
+  margin-bottom: 20px;
+  padding: 0 15px;
+  text-align: center;
+}
+
+button {
+    background-color: #0080ff;
+    border: none;
+    border-radius: 4px;
+    color: #fff;
+    font-size: 14px;
+    height: 40px;
+    line-height: 40px;
+
+    &:after {
+      border: none;
+    }
+
+    &:active {
+      background-color: #fff;
+      border: 1px solid #0080ff;
+      color: #0080ff;
+    }
+}
+</style>
--- src/pages/bind/index.vue
+++ src/pages/bind/index.vue
@@ -14,3 +14,26 @@ export default {
   name: 'Bind'
 }
 </script>
+
+<style lang="scss" scoped>
+#bind {
+  height: 100%;
+
+  .background {
+    background-color: #0080ff;
+    height: 100px;
+  }
+
+  .bind-student-form {
+    background-color: #fff;
+    border-radius: 4px;
+    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, .1);
+    box-sizing: border-box;
+    margin: 0 20px;
+    padding: 20px;
+    position: absolute;
+    top: 20px;
+    width: calc(100% - 40px);
+  }
+}
+</style>
