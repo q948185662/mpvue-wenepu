acce52122b9e67f795797c6b3ddc819ae09e5032
superchijinpeng <superchijinpeng@gmail.com>
2018-03-13 09:26:36 +0800

    feat(vuex): add get app token action

--- src/store/modules/authentication.js
+++ src/store/modules/authentication.js
@@ -1,4 +1,5 @@
-import { getStorageSync } from '../../utils/storage'
+import { getStorageSync, setStorage } from '../../utils/storage'
+import { getAppToken } from '../../api/authentication'
 
 const state = {
   appToken: getStorageSync('appToken') || '',
@@ -6,7 +7,15 @@ const state = {
 }
 
 const actions = {
-
+  async getAppToken ({ commit }, authenticationForm) {
+    try {
+      const { data: appToken } = await getAppToken(authenticationForm)
+      await setStorage('appToken', appToken)
+      commit('SET_APP_TOKEN', appToken)
+    } catch (error) {
+      return Promise.reject(error)
+    }
+  }
 }
 
 const mutations = {
