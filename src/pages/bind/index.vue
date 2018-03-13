826376e1f01077d39d01cb17558cd7d876613cfe
superchijinpeng <superchijinpeng@gmail.com>
2018-03-13 15:40:46 +0800

    feat(page layout): add bind page layout

--- src/pages/bind/index.vue
+++ src/pages/bind/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <div id="bind"></div>
+  <div id="bind">
+    <div class="background"></div>
+    <div class="bind-student-form">
+      <input type="text" placeholder="学号"/>
+      <input type="password" placeholder="密码"/>
+      <button>绑定</button>
+    </div>
+  </div>
 </template>
 
 <script>
