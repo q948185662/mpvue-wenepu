3420b45290a5f0aba312296f0d2a77920abfe7b2
superchijinpeng <superchijinpeng@gmail.com>
2018-03-13 14:46:45 +0800

    feat(home page): add is bind student method

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -3,7 +3,26 @@
 </template>
 
 <script>
+import { getStorage } from '../../utils/storage'
+import { navigateTo } from '../../utils/navigate'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  methods: {
+    // 判断是否绑定学号
+    async isBindStudent () {
+      try {
+        // 通过 Key 'student' 从 Storage 中获取 student, 没获取到时会抛出异常
+        await getStorage('student')
+      } catch (error) {
+        // 没获取到 student 说明没有绑定学号, 跳转到绑定学号页面
+        navigateTo('/pages/bind/main')
+      }
+    }
+  },
+  onShow () {
+    // 页面显示时判断是否绑定学号
+    this.isBindStudent()
+  }
 }
 </script>
