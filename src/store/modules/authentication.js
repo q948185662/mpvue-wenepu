f5d0c3d2e7f081b985785df9d07e5c7f1339e81d
superchijinpeng <superchijinpeng@gmail.com>
2018-03-13 09:29:23 +0800

    feat(vuex): add set web token mutation

--- src/store/modules/authentication.js
+++ src/store/modules/authentication.js
@@ -21,6 +21,9 @@ const actions = {
 const mutations = {
   'SET_APP_TOKEN' (state, appToken) {
     state.appToken = appToken
+  },
+  'SET_WEB_TOKEN' (state, webToken) {
+    state.webToken = webToken
   }
 }
 
