8adf6f410b81e69ec7298f16dbfab959225d7bb4
superchijinpeng <superchijinpeng@gmail.com>
2018-03-13 09:26:11 +0800

    feat(util): add set storage method

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -16,7 +16,23 @@ const getStorageSync = key => {
   return wx.getStorageSync(key)
 }
 
+const setStorage = (key, data) => {
+  return new Promise((resolve, reject) => {
+    wx.setStorage({
+      key,
+      data,
+      success ({ data }) {
+        resolve(data)
+      },
+      fail (error) {
+        reject(error)
+      }
+    })
+  })
+}
+
 export {
   getStorage,
-  getStorageSync
+  getStorageSync,
+  setStorage
 }
