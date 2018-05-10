bd9a9e9e1f4e1d06c8a572039391598ba5ab8075
superchijinpeng <superchijinpeng@gmail.com>
2018-05-10 13:44:57 +0800

    feat(util): add get storage util

---
+++ src/utils/storage.js
@@ -0,0 +1,19 @@
+import wx from './wx'
+
+const getStorage = key => {
+  return new Promise((resolve, reject) => {
+    wx.getStorage({
+      key,
+      success ({ data }) {
+        resolve(data)
+      },
+      fail (error) {
+        reject(error)
+      }
+    })
+  })
+}
+
+export {
+  getStorage
+}
