8d4b79512f8b4a93d9be8faf23979bc420fb1432
superchijinpeng <superchijinpeng@gmail.com>
2018-05-10 15:34:14 +0800

    feat(layout): add user info layout

--- src/pages/more/index.vue
+++ src/pages/more/index.vue
@@ -1,5 +1,13 @@
 <template>
-    <div id="more"></div>
+    <div id="more">
+        <div class="user-info">
+            <open-data type="userAvatarUrl" class="avatar"></open-data>
+            <div class="name">
+                <div class="school-number"></div>
+                <open-data type="userNickName" class="wechat-nick-name"></open-data>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
