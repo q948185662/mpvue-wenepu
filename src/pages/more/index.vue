5d76b5bb128eb6c58d6511cd4b970d8a295a4654
superchijinpeng <superchijinpeng@gmail.com>
2018-05-10 15:35:54 +0800

    style(user info): add user info style

--- src/pages/more/index.vue
+++ src/pages/more/index.vue
@@ -15,3 +15,44 @@ export default {
   name: 'More'
 }
 </script>
+
+<style lang="scss">
+page {
+  background-color: #f8f8f8;
+
+  #more {
+
+        .user-info {
+            background-color: #0080ff;
+            padding: 20px;
+
+            .avatar {
+                border-radius: 50%;
+                box-shadow: 0 2px 12px 0 rgba(0, 0, 0, .1);
+                display: block;
+                height: 100px;
+                margin: 0 auto;
+                overflow: hidden;
+                width: 100px;
+            }
+
+            .name {
+                display: flex;
+                font-size: 14px;
+                height: 40px;
+                line-height: 40px;
+                color: #fff;
+
+                .school-number {
+                    flex: 1;
+                }
+
+                .wechat-nick-name {
+                    flex: 1;
+                    text-align: right;
+                }
+            }
+        }
+    }
+}
+</style>
