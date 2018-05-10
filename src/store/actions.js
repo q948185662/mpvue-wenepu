0049e3705765ad5eb26159bb989843536e3d4294
superchijinpeng <superchijinpeng@gmail.com>
2018-05-10 14:27:07 +0800

    feat(vuex): add get web token action

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,4 +1,4 @@
-import { getAppToken } from '../api/authentication'
+import { getAppToken, getWebToken } from '../api/authentication'
 import { setStorage } from '../utils/storage'
 
 export default {
@@ -7,6 +7,15 @@ export default {
       const { appToken } = await getAppToken(user)
       await setStorage('appToken', appToken)
       commit('SET_APP_TOKEN', appToken)
+    } catch (error) {
+      return Promise.reject(error)
+    }
+  },
+  async getWebToken ({ commit }, user) {
+    try {
+      const { webToken } = await getWebToken(user)
+      await setStorage('webToken', webToken)
+      commit('SET_WEB_TOKEN', webToken)
     } catch (error) {
       return Promise.reject(error)
     }
