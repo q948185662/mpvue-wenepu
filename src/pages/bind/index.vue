fa644f448da07e3abcdcca5267738c1dab23cdc0
superchijinpeng <superchijinpeng@gmail.com>
2018-05-10 13:48:28 +0800

    feat(layout): add bind user form layout

--- src/pages/bind/index.vue
+++ src/pages/bind/index.vue
@@ -1,5 +1,12 @@
 <template>
-    <div id="bind"></div>
+    <div id="bind">
+        <div class="background"></div>
+        <div class="bind-user-form">
+            <input type="text" placeholder="请输入学号"/>
+            <input type="password" placeholder="请输入密码"/>
+            <button>绑定</button>
+        </div>
+    </div>
 </template>
 
 <script>
