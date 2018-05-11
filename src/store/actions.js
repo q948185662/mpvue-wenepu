3e469e99aa8fbe4b693e2729bf6b2f1efa18fd9d
superchijinpeng <superchijinpeng@gmail.com>
2018-05-11 16:01:44 +0800

    feat(vuex): add get semester list action

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,5 +1,6 @@
 import { getAppToken, getWebToken } from '../api/authentication'
 import { setStorage } from '../utils/storage'
+import { getSemesterList } from '../api/score'
 
 export default {
   async getAppToken ({ commit }, user) {
@@ -30,6 +31,30 @@ export default {
       commit('SET_USER', user)
     } catch (error) {
       return Promise.reject(error)
+    }
+  },
+  async getSemesterList ({ commit }) {
+    try {
+      const { webToken } = this.state
+      const { semesterList } = await getSemesterList(webToken)
+      await setStorage('semesterList', semesterList)
+      commit('SET_SEMESTER_LIST', semesterList)
+    } catch (error) {
+      const message = error.substring(0, error.indexOf('，'))
+      if (message === 'web token 错误') {
+        try {
+          const { user } = this.state
+          await this.dispatch('getWebToken', user)
+          const { webToken } = this.state
+          const { semesterList } = await getSemesterList(webToken)
+          await setStorage('semesterList', semesterList)
+          commit('SET_SEMESTER_LIST', semesterList)
+        } catch (error) {
+          return Promise.reject(error)
+        }
+      } else {
+        return Promise.reject(error)
+      }
     }
   }
 }
