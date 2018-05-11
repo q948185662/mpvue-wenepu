7c2910086047175b6f0be16699387ff6e52bffee
superchijinpeng <superchijinpeng@gmail.com>
2018-05-11 14:27:46 +0800

    feat(layout): add home navigator layout

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,5 +1,21 @@
 <template>
-    <div id="home"></div>
+    <div id="home">
+        <div class="background"></div>
+        <div class="navigator">
+            <navigator url="/pages/score/main" hover-class="navigator-hover">
+                <img src="https://resource.mignon.me/wenepu/images/icon/home/menu/score.png"/>
+                <div class="label">成绩</div>
+            </navigator>
+            <navigator hover-class="navigator-hover">
+                <img src="https://resource.mignon.me/wenepu/images/icon/home/menu/course-table.png"/>
+                <div class="label">课程表</div>
+            </navigator>
+            <navigator hover-class="navigator-hover">
+                <img src="https://resource.mignon.me/wenepu/images/icon/home/menu/free-classroom.png"/>
+                <div class="label">空教室</div>
+            </navigator>
+        </div>
+    </div>
 </template>
 
 <script>
