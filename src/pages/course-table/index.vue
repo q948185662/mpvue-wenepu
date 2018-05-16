97d146db6e132d374202dedbb45887452c0983fc
superchijinpeng <superchijinpeng@gmail.com>
2018-05-16 16:43:46 +0800

    feat(method): add get current week method

--- src/pages/course-table/index.vue
+++ src/pages/course-table/index.vue
@@ -3,7 +3,52 @@
 </template>
 
 <script>
+import { showNavigationBarLoading, hideNavigationBarLoading } from '../../utils/loading'
+import { getStorage, clearStorage } from '../../utils/storage'
+import { showToast } from '../../utils/toast'
+import { navigateBack, reLaunch } from '../../utils/navigate'
+import { getCurrentDate, isInDate } from '../../utils/date'
+
 export default {
-  name: 'CourseTable'
+  name: 'CourseTable',
+  methods: {
+    async getCurrentWeek () {
+      showNavigationBarLoading('获取当前周中')
+      try {
+        await this.$store.dispatch('getCurrentWeek')
+        hideNavigationBarLoading('课程表')
+      } catch (error) {
+        hideNavigationBarLoading('课程表')
+        const message = error.substring(0, error.indexOf('，'))
+        if (message === '服务器错误') {
+          await showToast({
+            title: '服务器错误，获取当前周失败，请稍后再试！'
+          })
+          navigateBack('/pages/home/main')
+        } else {
+          await showToast({
+            title: '学号或密码错误，请重新绑定！'
+          })
+          clearStorage().then(() => {
+            reLaunch('/pages/home/main')
+          })
+        }
+      }
+    },
+    async isGotCurrentWeek () {
+      try {
+        const { weekStartDate, weekEndDate } = await getStorage('currentWeek')
+        const currentDate = getCurrentDate()
+        if (!isInDate(weekStartDate, weekEndDate, currentDate)) {
+          await this.getCurrentWeek()
+        }
+      } catch (error) {
+        await this.getCurrentWeek()
+      }
+    }
+  },
+  onShow () {
+    this.getCurrentWeek()
+  }
 }
 </script>
